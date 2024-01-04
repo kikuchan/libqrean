cf0d496a2fd897f728d908d9451b66581d17b424
kikuchan <kikuchan98@gmail.com>
2024-01-04 21:18:11 +0900

    Update and fix tests

--- tests/test.sh
+++ tests/test.sh
@@ -5,7 +5,7 @@ QREAN=${QREAN:-../cli/qrean/qrean}
 QREAN_DETECT=${QREAN_DETECT:-../cli/qrean/qrean-detect}
 error=0
 
-function check() {
+check() {
 	echo -n "$2 ... "
 	if diff -q "$1" "$2" > /dev/null; then
 		printf "\033[32mPASS\033[m\n"
@@ -15,6 +15,7 @@ function check() {
 	fi
 }
 
-${QREAN} -t rmqr -f TXT Hello | check - rmqr-1.txt
+${QREAN} -t rmqr -f txt Hello | check - rmqr-1.txt
+${QREAN} -t tqr -f txt 000000000000 | check - tqr-000000000000.txt
 
 exit ${error}
